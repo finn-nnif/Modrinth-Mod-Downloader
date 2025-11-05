1f81898a6052f1cbe6f7e4624c1ffaa0e350b08a
finn-nnif <finnsullivank@gmail.com>
2025-11-05 20:54:57 +0800

    Working example and placeholder mod list with supportive comments

--- config/lists.js
+++ config/lists.js
@@ -2,5 +2,16 @@
 export const vanilla_qol = {
     version: ['1.21.10', '1.21.9', '1.21.8'],
     loader: ['fabric'],
-    mods: ['sodium', 'lithium'],
+    mods: ['sodium', 'lithium', 'bobby', 'appleskin', 'borderless-fullscreen', 'collective', 'essential', 'ferrite_core', 'fullbrightnesstoggle', 'iris', 'journeymap',
+           'krypton', 'lambdynamiclights', 'modmenu', 'morechathistory', 'noxesium', 'phases-discord-rich-presence', 'status-effect-bars', 'vmp-fabric', 
+           'simple-voice-chat', 'worldedit'
+          ],
 };
+
+/* EXAMPLE MOD LIST
+export const example = {
+    version: ['1.21.10'],   // Type in any pre-existing version, typically in the format x.x.x
+    loader: ['fabric'],     // E.g. fabric, forge, quilt, etc. (Lowercase Only)
+    mods: ['example'],      // As explained in the README.md utilise the name on the mod page in the url which comes before the .../[HERE]?version=...
+};
+*/
