f92d6d3bcd8304f3661e63662819f83b7c8e5d31
finn-nnif <finnsullivank@gmail.com>
2025-11-05 20:57:15 +0800

    Sodium Template Added

--- config/lists.js
+++ config/lists.js
@@ -1,4 +1,10 @@
 
+export const sodium = {
+    version: ['1.21.10', '1.21.9', '1.21.8'],
+    loader: ['fabric'],
+    mods: ['sodium'],
+};
+
 export const vanilla_qol = {
     version: ['1.21.10', '1.21.9', '1.21.8'],
     loader: ['fabric'],
